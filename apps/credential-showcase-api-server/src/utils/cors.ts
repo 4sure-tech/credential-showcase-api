d342c626fb0bcbcc5cc1607913686fa2f554d949
Ronaldo Lima <ronal2do@gmail.com>
2025-03-18 13:29:42 +0100

    chore: update CORS settings and port configuration in environment files

--- apps/credential-showcase-api-server/src/utils/cors.ts
+++ apps/credential-showcase-api-server/src/utils/cors.ts
@@ -1,14 +1,18 @@
 import type { CorsOptions } from 'cors'
 
-export const corsDisabled = process.env.CORS_DISABLED === 'true'
-const allowOrigins = process.env.ALLOW_ORIGINS?.split(',') ?? ['*']
-const allowMethods = process.env.ALLOW_METHODS?.split(',') ?? ['GET', 'POST', 'PUT', 'DELETE']
-const allowHeaders = process.env.ALLOW_HEADERS?.split(',') ?? ['Content-Type', 'Authorization']
-const allowCredentials = process.env.ALLOW_CREDENTIALS === 'true'
 
 export const corsOptions: CorsOptions = {
-  origin: allowOrigins,
-  methods: allowMethods,
-  allowedHeaders: allowHeaders,
-  credentials: allowCredentials,
+  origin: (requestOrigin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    const allowedOrigins = process.env.ALLOW_ORIGINS?.split(',') ?? ['*']
+
+    if (!requestOrigin || process.env.CORS_DISABLED === 'true') return callback(null, true)
+    if (allowedOrigins.indexOf(requestOrigin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  },
+  methods: process.env.ALLOW_METHODS?.split(',') ?? ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: process.env.ALLOW_HEADERS?.split(',') ?? ['Content-Type', 'Authorization', 'X-Requested-With'],
+  credentials: process.env.ALLOW_CREDENTIALS === 'true',
 }
