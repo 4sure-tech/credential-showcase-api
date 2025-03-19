38b6656867f5318d57d25f43a5840606a1ea9c52
Zoe Maas <zmaas@4sure.tech>
2025-03-19 15:17:24 +0100

    chore: removed the action type check to avoid api changes

--- apps/credential-showcase-api-server/src/database/migrations/0003_credential-showcase-api.sql
+++ apps/credential-showcase-api-server/src/database/migrations/0003_credential-showcase-api.sql
@@ -1,7 +1,2 @@
-CREATE TYPE "public"."StepActionType" AS ENUM('ISSUANCE', 'PRESENTATION');--> statement-breakpoint
-ALTER TABLE "stepAction" ALTER COLUMN "action_type" TYPE "public"."StepActionType";--> statement-breakpoint
 ALTER TABLE "stepAction" ADD COLUMN "credential_representation_id" uuid;--> statement-breakpoint
-ALTER TABLE "stepAction" ADD CONSTRAINT "stepAction_credential_representation_id_fk" FOREIGN KEY ("credential_representation_id") REFERENCES "public"."credentialRepresentation"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "stepAction" ADD CONSTRAINT "stepAction_type_check" CHECK (
-    (action_type = 'PRESENTATION') OR
-    (action_type = 'ISSUANCE' AND "credential_representaion_id" IS NOT NULL)
+ALTER TABLE "stepAction" ADD CONSTRAINT "stepAction_credential_representation_id_fk" FOREIGN KEY ("credential_representation_id") REFERENCES "public"."credentialRepresentation"("id") ON DELETE no action ON UPDATE no action;
